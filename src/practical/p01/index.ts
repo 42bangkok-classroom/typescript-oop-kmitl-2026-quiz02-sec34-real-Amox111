5ee275598a4ac05e71f97ec57957b548427620bd
Amox111 <ruxkwarmmana233@gmail.com>
2026-01-16 19:58:07 +0700

    l

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,52 @@
-export function getPostalAddress() {}
+import axios from "axios";
+
+interface Geo {
+    lat: string;
+    lng: string;
+}
+
+interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+}
+
+interface ApiUser {
+    id: number;
+    name: string;
+    phone: string;
+    address: Address;
+}
+
+interface UserResult {
+    id: number;
+    name: string;
+    phone: string;
+    address: Address | null;
+}
+
+const filterUserById = async (id: number): Promise<UserResult | string> => {
+  try {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const response = await axios.get<ApiUser[]>(url);
+    const foundUser = response.data.find((u) => u.id === id);
+
+    if (!foundUser) {
+      return "Invalid id";
+    }
+
+    return {
+      id: foundUser.id,
+      name: foundUser.name,
+      phone: foundUser.phone,
+      address: foundUser.address || null
+    };
+
+  } catch (error) {
+    return "Invalid id";
+  }
+};
+
+export default filterUserById;
